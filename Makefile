fdcadff4e02de50660decd3da886c5848cc150ed
Diogo Almeida <dha21@inf.ufpr.br>
2023-06-11 17:08:40 -0300

    estava pegando

---
+++ Makefile
@@ -0,0 +1,18 @@
+all:
+	echo Passe o numero da máquina!
+
+1: game.py
+	clear
+	python3 game.py -m 1
+
+2: game.py
+	clear
+	python3 game.py -m 2
+
+3: game.py
+	clear
+	python3 game.py -m 3
+
+4: game.py
+	clear
+	python3 game.py -m 4
--- game.py
+++ game.py
@@ -108,6 +108,8 @@ def receiveMessage(socket):
     return message
 
 # ---------- GAME FUNCTIONS ----------
+def printLine():
+    print('-----------------------------------------------')
 
 def createDeck():
     deck = []
@@ -123,9 +125,102 @@ def createDeck():
 
     return deck
 
-# Dar uma carta
-# Passar a vez
-# Jogar quantidade de cartas
+def updateGameState(game_state, num_cards, min_card, passed, machine, rnd):
+    game_state["num_cards"] = num_cards
+    game_state["min_card"] = min_card
+    if(passed == True):
+        game_state["round_passed"] += 10**(machine-1)
+    else:
+        game_state["last_played"] = machine
+    game_state["played"] += 10**(machine-1)
+    game_state["round"] = rnd
+    return game_state
+
+def verifyPlay(hand, min_card, num_cards):
+    for i in range(min_card):
+        if(hand.count(i+1) >= num_cards):
+            return True
+    return False
+
+def playSet(game_state, hand, machine):
+    valid_play = False
+    valid_choice = False
+    play = 0
+    
+    # New turn
+    if(game_state["end_turn"] == True):
+        game_state["end_turn"] = False
+    
+    # End turn
+    if(game_state["round_passed"] == 1111):
+        game_state["turn"] += 1
+        game_state["end_turn"] = True
+        game_state["round"] = 0
+        game_state["played"] = 0
+        game_state["round_passed"] = 0
+    return game_state
+    
+    # New round reset
+    if(game_state["played"] == 1111):
+        game_state["round"] += 1
+        game_state["played"] = 0
+        game_state["round_passed"] = 0
+    
+    print(f'Você precisa jogar {game_state["num_cards"]} cartas menores ou igual a {game_state["min_card"]}.')
+    while not valid_play:
+        printLine()
+        print(hand)
+        # First play
+        if (game_state["round"] == 0 and game_state["played"] == 0):
+                num_cards = int(input('Escolha o número de cartas que deseja jogar: '))
+                min_card = int(input('Escolha a carta que deseja jogar: '))
+                printLine()
+                if num_cards > hand.count(min_card):
+                    print('Jogada inválida!')
+                    continue
+                
+                for i in range(num_cards):
+                    hand.remove(min_card)
+                    
+                game_state = updateGameState(game_state, num_cards, min_card, False, machine, game_state["round"])
+                valid_play = True
+        # Next plays
+        else:
+            play = int(input('(1) Jogar uma carta\n(2) Passar a vez\nEscolha sua jogada: '))
+            while(valid_choice == False):
+                if(play != 1 and play != 2):
+                    print('Escolha uma opção válida!')
+                elif(play == 1 and verifyPlay(hand, game_state["min_card"], game_state["num_cards"]) == False):
+                    print('Você não tem cartas para jogar! Escolha passar a vez.')
+                else:
+                    valid_choice = True
+                    continue
+                play = int(input('(1) Jogar uma carta\n(2) Passar a vez\nEscolha sua jogada: '))
+            
+            if play == 1:
+                min_card = int(input('Escolha a carta que deseja jogar: '))
+                printLine()
+                print(hand.count(min_card))
+                while valid_play == False:
+                    if(hand.count(min_card) < game_state["num_cards"]):
+                        print(f'Jogada inválida! Você precisa jogar um conjunto de {game_state["num_cards"]} cartas.')
+                    elif(min_card > game_state["min_card"]):
+                        print(f'Jogada inválida! A carta tem que ser menor que {game_state["min_card"]}.')
+                    else:
+                        valid_play = True
+                        continue
+                    min_card = int(input('Escolha a carta que deseja jogar: '))
+                
+                for i in range(game_state["num_cards"]):
+                    hand.remove(min_card)
+                
+                game_state = updateGameState(game_state, game_state["num_cards"], min_card, False, machine, game_state["round"])
+                
+            elif play == 2:
+                game_state = updateGameState(game_state, game_state["num_cards"], game_state["min_card"], True, machine, game_state["round"])
+                valid_play = True
+
+    return game_state
 
 # ---------- MAIN PROGRAM ----------
 
@@ -135,11 +230,22 @@ if __name__ == "__main__":
     bat = 0
     hand = []
     deck = []
+    game_state = {
+        "num_cards": 0,
+        "min_card": 0,
+        "turn": 0,
+        "round": 0,
+        "round_passed": 0,
+        "played": 0,
+        "last": 0,
+        "dalmuti": 4,
+        "end_turn": False
+    }
     getScriptArguments(sys.argv)
     
-
     # Deal setup
     if(machine == 4):
+        bat = 1
         deck = createDeck()
         
     machines = readConfigure()
@@ -147,19 +253,17 @@ if __name__ == "__main__":
         print('Use mais máquinas!')
         quit(2)
 
-    if(machine == 1):
-        bat = 1
-
     openSockets(machines, machine)
 
     while True:
-        # Setup state
+        # ----- Setup state ----- 
         if(state == 0):
             # Other players
-            if (machine != 4):
+            if (machine != game_state["dalmuti"]):
                 message = receiveMessage(left_socket)
-                if(message["destiny"] == machine):
+                if(message["destiny"] == machine or message["destiny"] == 0):
                     if(message["play"]["empty_deck"] == 1):
+                        hand.sort()
                         state = 1
                     else:
                         hand.append(message["play"]["deal_card"])
@@ -167,7 +271,7 @@ if __name__ == "__main__":
                 sendMessage(right_socket, send_address, message)          
             
             # The deal
-            elif(machine == 4):
+            elif(machine == game_state["dalmuti"]):
                 while(deck != []):
                     hand.append(deck.pop())
                     for i in [1 , 2 , 3]:
@@ -175,32 +279,41 @@ if __name__ == "__main__":
                         message = create_message(machine, {"empty_deck": 0, "deal_card": card}, i)
                         sendMessage(right_socket, send_address, message)
                         message = receiveMessage(left_socket)
-                        
-                for i in [1 , 2 , 3]:
-                    message = create_message(machine, {"empty_deck": 1}, i)
-                    sendMessage(right_socket, send_address, message)
-                    message = receiveMessage(left_socket)
-                    print(i, message)
+                
+                message = create_message(machine, {"empty_deck": 1}, 0) # send to everyone
+                sendMessage(right_socket, send_address, message)
+                message = receiveMessage(left_socket)
                 state = 1
 
                         
             
-        # Playing state
-        elif(state == 1):
-            print('Mudou de estado!', hand)
-            break
-        
+        # ----- Playing state ----- 
+        # Primeiro jogador joga uma quantidade de cartas de mesmo número
+        # Jogadores seguintes devem jogar a mesma quantidade de cartas de um número igual ou menor
+        # Rodada acaba quando todos passarem a vez
+        elif(state == 1):                   
             if(bat == 0):
                 message = receiveMessage(left_socket)
-                print('Mensagem recebida!:', message)
-                if(message["receive"] == 1111):
-                    break
-                message = confirmReceive(message, machine)
-                sendMessage(right_socket, send_address, message)
+                print('mensagem recebida!', message)
+                game_state = message["play"]["game_state"]
+                if(message["destiny"] == machine):
+                    bat = 1
+                else:
+                    sendMessage(right_socket, send_address, message)
+            
+                # message = confirmReceive(message, machine)
+                # sendMessage(right_socket, send_address, message)
             else:
-                message = create_message(machine, 1)
+                game_state = playSet(game_state, hand, machine)
+                if(game_state["end_turn"] == True):
+                    message = create_message(machine, {"game_state": game_state, "pass_token": True}, game_state["last_played"])
+                else:
+                    if(machine+1 > machines[0]):
+                        message = create_message(machine, {"game_state": game_state, "pass_token": True}, 1)
+                    else:
+                        message = create_message(machine, {"game_state": game_state, "pass_token": True}, machine + 1)
                 sendMessage(right_socket, send_address, message)
-                print('Mensagem enviada!', message)
-                bat = 0
+                print('mensagem enviada!', message)
+y                bat = 0
 
-        
+       
